fac3fb20261ab3dfa3723b6daa0452d87bfa22a4
璀璨梦星 <157385609+cuicanmengxing@users.noreply.github.com>
2024-08-30 14:28:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 #
 # https://github.com/P3TERX/Actions-OpenWrt
@@ -10,29 +11,11 @@
 # See /LICENSE for more information.
 #
 
-git pull
+# Modify default IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
-echo "src-git small8 https://github.com/kenzok8/small-package" >> feeds.conf.default
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-./scripts/feeds clean
-./scripts/feeds update -a
-
-\rm -rf ./feeds/luci/applications/{luci-app-passwall,luci-app-smartdns,luci-app-ddns-go,luci-app-rclone,luci-app-ssr-plus,luci-app-vssr}
-\rm -rf ./feeds/luci/themes/luci-theme-argon
-\rm -rf ./feeds/packages/net/{haproxy,xray-core,xray-plugin,mosdns,smartdns,ddns-go,dns2tcp,dns2socks}
-\rm -rf ./feeds/small8/{ppp,firewall,dae,daed,daed-next,libnftnl,nftables,dnsmasq}
-
-if [[ -d ./feeds/packages/lang/golang ]]; then
-	\rm -rf ./feeds/packages/lang/golang
-	git clone https://github.com/sbwml/packages_lang_golang -b 22.x ./feeds/packages/lang/golang
-fi
-
-./scripts/feeds update -i
-./scripts/feeds install -f -ap packages
-./scripts/feeds install -f -ap luci
-./scripts/feeds install -f -ap routing
-./scripts/feeds install -f -ap telephony
-
-./scripts/feeds install -p small8 -f luci-app-adguardhome xray-core xray-plugin dns2tcp dns2socks haproxy \
-luci-app-passwall luci-app-mosdns luci-app-smartdns luci-app-ddns-go luci-app-cloudflarespeedtest taskd \
-luci-lib-xterm luci-lib-taskd luci-app-store quickstart luci-app-quickstart luci-app-istorex luci-theme-argon
+# Modify hostname
+sed -i 's/OpenWrt/S1-Router/g' package/base-files/files/bin/config_generate
